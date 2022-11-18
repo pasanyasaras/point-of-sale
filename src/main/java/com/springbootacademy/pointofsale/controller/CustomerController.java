50f18a460e5574365504a4a17eb98a0c675297f7
pasanyasaras <pasansmart@gmail.com>
2022-11-19 02:16:09 +0530

    Adding controllers

--- src/main/java/com/springbootacademy/pointofsale/controller/CustomerController.java
+++ src/main/java/com/springbootacademy/pointofsale/controller/CustomerController.java
@@ -1,16 +1,14 @@
 package com.springbootacademy.pointofsale.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@RequestMapping("api/v1/test")
-public class TestController {
+@RequestMapping("api/v1/customer")
+public class CustomerController {
 
-    @GetMapping
     public void getMyText(){
         String myText = "This is my springboot app";
         System.out.println(myText);
