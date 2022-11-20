8a23d71536780d86cca867e067e9ad605cfe6c4f
pasanyasaras <pasansmart@gmail.com>
2022-11-20 12:57:30 +0530

    Adding entity and dto

---
+++ src/main/java/com/springbootacademy/pointofsale/dto/CustomerDTO.java
@@ -0,0 +1,98 @@
+package com.springbootacademy.pointofsale.dto;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import java.util.ArrayList;
+
+public class CustomerDTO {
+    private int customerId;
+    private String customerName;
+    private String customerAddress;
+    private double customerSalary;
+    private String nic;
+    private ArrayList contactNumber;
+    private boolean activeState;
+
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(int customerId, String customerName, String customerAddress, double customerSalary, String nic, ArrayList contactNumber, boolean activeState) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerSalary = customerSalary;
+        this.nic = nic;
+        this.contactNumber = contactNumber;
+        this.activeState = activeState;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public double getCustomerSalary() {
+        return customerSalary;
+    }
+
+    public void setCustomerSalary(double customerSalary) {
+        this.customerSalary = customerSalary;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public ArrayList getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(ArrayList contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public boolean isActiveState() {
+        return activeState;
+    }
+
+    public void setActiveState(boolean activeState) {
+        this.activeState = activeState;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDTO{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", customerSalary=" + customerSalary +
+                ", nic='" + nic + '\'' +
+                ", contactNumber=" + contactNumber +
+                ", activeState=" + activeState +
+                '}';
+    }
+}
---
+++ src/main/java/com/springbootacademy/pointofsale/entity/Customer.java
@@ -0,0 +1,126 @@
+package com.springbootacademy.pointofsale.entity;
+
+
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "customer")
+@TypeDefs({
+        @TypeDef(name="json", typeClass = JsonType.class)
+})
+public class Customer {
+
+    @Id
+    @Column(name="customer_id", length = 45)
+    private int customerId;
+
+    @Column(name="customer_name", length = 100, nullable = false)
+    private String customerName;
+
+    @Column(name="customer_address")
+    private String customerAddress;
+
+    @Column(name="customer_salary")
+    private double customerSalary;
+
+    @Column(name="nic")
+    private String nic;
+
+    @Type(type="json")
+    @Column(name="contact_numbers", columnDefinition = "json")
+    private ArrayList contactNumber;
+
+    @Column(name="active_state", columnDefinition = "TINYINT default 1")
+    private boolean activeState;
+
+    public Customer(){
+    }
+
+    public Customer(int customerId, String customerName, String customerAddress, double customerSalary, String nic, ArrayList contactNumber, boolean activeState) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerSalary = customerSalary;
+        this.nic = nic;
+        this.contactNumber = contactNumber;
+        this.activeState = activeState;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public double getCustomerSalary() {
+        return customerSalary;
+    }
+
+    public void setCustomerSalary(double customerSalary) {
+        this.customerSalary = customerSalary;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public ArrayList getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(ArrayList contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public boolean isActiveState() {
+        return activeState;
+    }
+
+    public void setActiveState(boolean activeState) {
+        this.activeState = activeState;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", customerSalary=" + customerSalary +
+                ", nic='" + nic + '\'' +
+                ", contactNumber=" + contactNumber +
+                ", activeState=" + activeState +
+                '}';
+    }
+}
