f7c9bdf8731764e6892ea3afd930679b9e3cf194
pasanyasaras <pasansmart@gmail.com>
2022-12-26 01:19:14 +0530

    Implementing AppWideExceptionHandler

--- src/main/java/com/springbootacademy/pointofsale/advisor/AppWideExceptionHandler.java
+++ src/main/java/com/springbootacademy/pointofsale/advisor/AppWideExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.springbootacademy.pointofsale.advisor;
 
+import com.springbootacademy.pointofsale.exception.NotFoundException;
+import com.springbootacademy.pointofsale.util.StandardResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class AppWideExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<StandardResponse> handleNotFoundException(NotFoundException e){
+        return new ResponseEntity<StandardResponse>(
+          new StandardResponse(404,"ERROR ",e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
 }
